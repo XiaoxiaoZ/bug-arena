570f0c4de54bf4a6841a4bb39cad56703df5a57e
XiaoxiaoZ <zxxzxxv@gmail.com>
2022-04-20 13:15:29 +0200

    add init location

--- generateMap.m
+++ generateMap.m
@@ -1,7 +1,6 @@
-function [outputArg1,outputArg2] = generateMap()
+function map = generateMap()
 %GENERATEMAP Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+map = [];
 end
 
--- main.m
+++ main.m
@@ -9,17 +9,17 @@ done1 = false;
 done2 = false;
 %map = generateMap();
 for i = 1:10000000 
-tic;
-%[path1, tree1, done1] = algorithm1(map, path1, tree1)
-pause(2)
-time1 = time1 + toc
-tic;
-%algorithm 2
-%[path2, tree2, done2] = algorithm2(map, path1, tree2)
-pause(3)
-time2 = time2 +toc
+    tic;
+    %[path1, tree1, done1] = algorithm1(map, path1, tree1, initLoc1)
+    pause(2)
+    time1 = time1 + toc
+    tic;
+    %algorithm 2
+    %[path2, tree2, done2] = algorithm2(map, path1, tree2, initLoc2)
+    pause(3)
+    time2 = time2 +toc
 
-if(done1 || done2)
-    break;
-end
+    if(done1 || done2)
+        break;
+    end
 end
