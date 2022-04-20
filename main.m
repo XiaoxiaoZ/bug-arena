54ad30d5b6a6b43fd96461064dc46d2134ce1372
XiaoxiaoZ <zxxzxxv@gmail.com>
2022-04-20 13:09:30 +0200

    Init, big structure

---
+++ main.m
@@ -0,0 +1,25 @@
+clear
+path1 = [];
+path2 = [];
+time1 = 0;
+time2 = 0;
+initLoc1 = [-100,-100];
+initLoc2 = [100,100];
+done1 = false;
+done2 = false;
+%map = generateMap();
+for i = 1:10000000 
+tic;
+%[path1, tree1, done1] = algorithm1(map, path1, tree1)
+pause(2)
+time1 = time1 + toc
+tic;
+%algorithm 2
+%[path2, tree2, done2] = algorithm2(map, path1, tree2)
+pause(3)
+time2 = time2 +toc
+
+if(done1 || done2)
+    break;
+end
+end
