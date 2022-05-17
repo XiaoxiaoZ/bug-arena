3131b2f46fb7765e5665facb5d3949b08df1e3b8
XiaoxiaoZ <zxxzxxv@gmail.com>
2022-05-17 15:10:43 +0200

    plot things

--- generateMap.m
+++ generateMap.m
@@ -4,7 +4,6 @@ function map = generateMap()
     map1 = mapMaze(10,10,'MapSize',[500 300],'MapResolution',1);
     map1 = occupancyMatrix(map1);
     map2 = flip(map1);
-    map = [map1;map2];
-    map = map(:,15:485);
+    map = ~[zeros(10,500);map1(16:285,:);zeros(10,500);map2(16:285,:);zeros(10,500)];
 end
 
--- main.m
+++ main.m
@@ -1,6 +1,9 @@
 clear
-path1 = [];
-path2 = [];
+start1 = [1,250];
+start2 = [570,250];
+goal = [285,250];
+path1 = [10,20;20,30;30,50];
+path2 = [100,200;200,300;300,500];
 time1 = 0;
 time2 = 0;
 initLoc1 = [-100,-100];
@@ -8,18 +11,37 @@ initLoc2 = [100,100];
 done1 = false;
 done2 = false;
 map = generateMap();
+map(183,500)
+fmap = figure;
+imshow(map')
+hold on
+scatter(start1(1,1),start1(1,2),'red')
+scatter(start2(1,1),start2(1,2),'blue')
+scatter(goal(1,1),goal(1,2))
+pause;
 for i = 1:10000000 
     tic;
-    %[path1, tree1, done1] = algorithm1(map, path1, tree1, initLoc1)
-    pause(2)
-    time1 = time1 + toc
+    %[path1, tree1, done1] = algorithm1(map, path1, tree1, initLoc1, start1, goal)
+    time1 = time1 + toc;
     tic;
     %algorithm 2
-    %[path2, tree2, done2] = algorithm2(map, path1, tree2, initLoc2)
-    pause(3)
-    time2 = time2 +toc
+    %[path2, tree2, done2] = algorithm2(map, path1, tree2, initLoc2, start1, goal)
+    time2 = time2 +toc;
+
+    %plot path
+    plot(path1(:,1),path1(:,2),'r')
+    plot(path2(:,1),path2(:,2),'b')
+    drawnow
+    text1 = sprintf('Player 1 time: %f \n',time1);
+    text2 = sprintf('Player 2 time: %f \n',time2);
+    clc
+    disp(text1);
+    disp(text2);
 
     if(done1 || done2)
         break;
     end
+
+
 end
+
