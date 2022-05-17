cb7eabcd50b9d1b4e3d80f8a9ec0aa7861c6885f
XiaoxiaoZ <zxxzxxv@gmail.com>
2022-05-17 11:30:59 +0200

    Update generateMap.m

--- generateMap.m
+++ generateMap.m
@@ -1,7 +1,7 @@
 function map = generateMap()
 %GENERATEMAP Summary of this function goes here
 %   Detailed explanation goes here
-    map = mapClutter(10,{'Box','Plus','Circle'},'MapSize',[50 50],'MapResolution',5);
+    map = mapClutter(100,{'Box','Plus','Circle'},'MapSize',[50 50],'MapResolution',5);
     map = occupancyMatrix(map);
 end
 
